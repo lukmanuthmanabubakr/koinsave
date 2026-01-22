67b3bd93af3e91048f2fca5709db6ad979172fde
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2026-01-22 20:37:45 +0100

    Update Swagger servers for production

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
+require("dotenv").config();
 const app = require("../src/app");
+const connectDB = require("../src/db/connect");
 
-module.exports = (req, res) => {
-  app(req, res);
+let ready = false;
+
+module.exports = async (req, res) => {
+  if (!ready) {
+    await connectDB(process.env.MONGO_URI);
+    ready = true;
+  }
+  return app(req, res);
 };
--- src/db/connect.js
+++ src/db/connect.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose");
 
+let isConnected = false;
+
 async function connectDB(uri) {
+  if (!uri) throw new Error("MONGO_URI is not set on the server");
+
+  if (isConnected) return;
+
   mongoose.set("strictQuery", true);
-  await mongoose.connect(uri);
+
+  await mongoose.connect(uri, {
+    serverSelectionTimeoutMS: 10000,
+  });
+
+  isConnected = true;
   console.log("MongoDB connected");
 }
 
