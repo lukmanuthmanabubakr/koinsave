81b02e5d65c1a0a582d8588bd7151c9eb0b811c2
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2026-01-22 20:02:06 +0100

    Fix Swagger path for Vercel

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
+
+const path = require("path");
+const fs = require("fs");
+
 const authRoutes = require("./modules/auth/auth.routes");
 const walletRoutes = require("./modules/wallet/wallet.routes");
 const transactionRoutes = require("./modules/transactions/transaction.routes");
-
-
-
 
 const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
@@ -19,16 +20,21 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-const swaggerDocument = YAML.load("./src/docs/swagger.yaml");
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const swaggerPath = path.join(__dirname, "docs", "swagger.yaml");
+
+if (fs.existsSync(swaggerPath)) {
+  const swaggerDocument = YAML.load(swaggerPath);
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+} else {
+  console.warn("Swagger file not found at:", swaggerPath);
+}
 
 app.get("/health", (req, res) => {
   res.status(200).json({ success: true, message: "API is running" });
 });
+
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/wallet", walletRoutes);
 app.use("/api/v1/transactions", transactionRoutes);
-
-
 
 module.exports = app;
